00612db71b26a5f22c03dea23e07e998abcd8963
Kayque Moraes <kaymoraesdias@gmail.com>
2023-09-19 18:43:46 -0300

    add: campo de texto

---
+++ src/componentes/CampoTexto/CampoTexto.js
@@ -0,0 +1,12 @@
+import './CampoTexto.css'
+
+export const CampoTexto = () => {
+
+    return (
+        <div className="campo-texto">
+            <label>Nome</label>
+            <input type="text" placeholder='Digite seu nome'/>
+        </div>
+    )
+
+}
