04c6fc06a0ec772ecd1ed9891e7f33737b7a84bc
Kayque Moraes <kaymoraesdias@gmail.com>
2023-09-19 16:46:04 -0300

    feat: add banner

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,14 @@
+import './Banner.css'
+
+function Banner(){
+    // JSX
+    return(
+
+        <header className='banner'>
+            <img src="/imagens/banner.png" alt="Banner da página"/>
+        </header>
+    )
+
+}
+
+export default Banner
