348bb5598ab75e4bfa2409c592d71659dad6b91d
Kayque Moraes <kaymoraesdias@gmail.com>
2023-09-20 20:57:49 -0300

    add: ::after no h3

--- src/componentes/Time/index.jsx
+++ src/componentes/Time/index.jsx
@@ -1,14 +1,16 @@
 import './Time.css';
+import TimeH3 from '../style'
 
 const Time = (props) => {
 
   return (
-    <div>
-        
-        <section className="time" style={{backgroundColor: props.corSecundaria}}>
-            <h3>{props.nome}</h3>
-        </section>
-    </div>
+
+      <section className="time" style={{backgroundColor: props.corSecundaria}}>
+        <TimeH3 corPrimaria={props.corPrimaria}>
+          <h3>{props.nome}</h3>
+        </TimeH3 >
+      </section>
+
   );
 };
 
---
+++ src/componentes/style.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const TimeH3 = styled.h3`
+::after{
+    background-color:${(props)=>props.corPrimaria}
+}`
+
+export default TimeH3
